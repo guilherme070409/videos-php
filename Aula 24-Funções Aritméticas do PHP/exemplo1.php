2b8914e3459738a3780051aa183e45577561616f
guilherme070409 <guigatomoura@gmail.com>
2025-07-24 15:54:09 -0300

    3.2

---
+++ Aula 24-Funções Aritméticas do PHP/exemplo1.php
@@ -0,0 +1,7 @@
+<?php 
+$r = abs(500);
+print("O valor absoluto é $r");
+
+$r = abs(-500);
+print("<p> O valor absoluto é $r");
+?>
---
+++ Aula 24-Funções Aritméticas do PHP/exemplo5.php
@@ -0,0 +1,7 @@
+<?php 
+$r = sqrt(81);
+print("A resposta é $r");
+
+$r = 81 ** (1/2);
+print("A resposta é $r");
+?>
