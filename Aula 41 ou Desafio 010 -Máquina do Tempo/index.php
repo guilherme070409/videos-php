e8c823bf1b7c57240e228fa60c08f080b43eb964
guilherme070409 <guigatomoura@gmail.com>
2025-07-28 10:13:16 -0300

    er

---
+++ Aula 41 ou Desafio 010 -Máquina do Tempo/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio php</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+    $atual =  date("Y");
+    $nasceu = $_GET['nasc'] ?? '2000';
+    $data = $_GET['ano'] ?? $atual;
+
+    ?>
+    <main>
+        <h1>Maquina do Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="nasc">Em que ano voçê nasceu?</label>
+        <input type="number" name="nasc" id="nasc" min="1900" max="<?=$atual?>" value="<?=$nasceu?>">
+        <label for="ano">Quer saber sua idade em que ano?(atualmente estamos em <strong><?=$data?></strong></label>
+        <input type="number" name="ano" id="ano" min="1900" value="<?=$atual?>">
+        <input type="submit" value="Qual será minha idade">    
+    </form>
+    </main>
+    <?php 
+    $idade = $data - $nasceu;
+
+    ?>
+    <section>
+        <h2>Resultado</h2>
+        <p>Quem nasceu em <?=$nasceu?> vai ter <strong><?=$idade?></strong> em <?=$data?></p>
+    </section>
+</body>
+</html>
---
+++ Aula 42 ou Desafio 011- Reajustar Preços/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $preco = $_REQUEST['preco'] ?? '0';
+    $reajuste = $_REQUEST['rea'] ?? '0';
+    ?>
+    <main>
+        <h1>Reajustador de Preços</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>">
+        <label for="preco">Preço do Produto R$</label>
+        <input type="number" name="preco" id="preco" min="0.10" step="0.01">
+
+        <label for="rea">Qual será o percentual de reajuste?(<strong><span id="p">?</span>%</strong>)</label>
+        <input type="range" name="rea" id="rea" min="0" max="100" step="1" oninput="mudaValor()">
+        <input type="submit" value="Reajustar">
+        </form>
+    </main>
+    <section>
+        <?php 
+            $aumento = $preco * $rea / 100;
+            $novo = $preco + $aumento;
+        ?>
+        <h2>Resultado do reajuste</h2>
+        <p>O produto que custava R$<?=number_format($preco, 2, ",", ",")?>, com <strong><?=$reajuste?> de aumento</strong> vai passar a custar <strong>R$<?=number_format($novo, 2,"," , ".")?></strong> a partir de agora.</p>
+    </section>
+    <script>
+        mudaValor()
+        function mudaValor() {
+            p.innerText = rea.value;
+
+        }
+    </script>
+</body>
+</html>
